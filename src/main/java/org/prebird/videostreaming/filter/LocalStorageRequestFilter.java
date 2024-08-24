6e288eff483bc4b3c4a71ef611bcd15a99553c52
prebird <78974381+prebird@users.noreply.github.com>
2024-08-24 17:42:55 +0900

    feat: static 리소스 요청에 대한 필터 추가

---
+++ src/main/java/org/prebird/videostreaming/filter/LocalStorageRequestFilter.java
@@ -0,0 +1,25 @@
+package org.prebird.videostreaming.filter;
+
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+public class LocalStorageRequestFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    if (request.getRequestURI().contains("/local-storage")) {
+      log.info("local storage filter passed");
+    }
+    filterChain.doFilter(request, response);
+  }
+}
