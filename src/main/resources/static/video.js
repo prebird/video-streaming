d4a88977b60d27ea8ac41fe4f7586aa41a93ee27
prebird <78974381+prebird@users.noreply.github.com>
2024-08-30 23:57:24 +0900

    feat: 서버 포트 변경

--- src/main/resources/static/video.js
+++ src/main/resources/static/video.js
@@ -1,7 +1,7 @@
 const video = {
   getVideo: (requestPrefix, name) => {
     const videoSource = document.getElementById('videoSource');
-    videoSource.src = `http://localhost:8080/${requestPrefix}/videos/${name}`; // 비디오 소스 URL 설정
+    videoSource.src = `http://localhost:7777/${requestPrefix}/videos/${name}`; // 비디오 소스 URL 설정
     document.getElementById('video').load(); // 새로운 소스를 로드
   }
 }
